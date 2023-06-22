6a0e15630dcfddffc8660afcc875f29f0796fab6
BalazsManus <82440273+BalazsManus@users.noreply.github.com>
2023-06-22 13:34:36 +0200

    yes

--- build.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-cd C:\Users\72661341100.DFAD\source\repos\Backwards
-echo fetching repo.
-git fetch origin-push
-echo pushing changes.
-git push --force-with-lease origin-push
-echo trying to pull.
-git pull origin-push
